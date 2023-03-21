347966c9f1090cc14692607aabf933a5bf08aeeb
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-21 16:23:12 -0300

    js fudido dando problema

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 function contar() {
-    
+
+    let ini = document.getElementById('txti') 
+    let fim = document.getelementById('txtf')
+    let passo = docment.getelementById('txtp')
+    let res = document.getelementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {  // se tiver  vazio
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+    }
+
 }
